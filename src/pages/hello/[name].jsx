dfafa146d60cfa79152f814bd620b962861800dc
Wenny <shine5983@gmail.com>
2023-05-01 18:25:22 +0800

    Use Next/Link to change the url and page

--- src/pages/hello/[name].jsx
+++ src/pages/hello/[name].jsx
@@ -2,9 +2,9 @@ import { useRouter } from "next/router";
 
 const Hello = () => {
   const router = useRouter();
-  const { helloId } = router.query;
+  const { name } = router.query;
   console.log(router.query);
-  return <p>Post: {helloId}</p>;
+  return <p>Hello: {name}</p>;
 };
 
 export default Hello;
